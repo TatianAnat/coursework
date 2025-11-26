d5fdcbea12c40ea750262efa01288ad4e35ac0d3
TatianAnat <TatianAnat@yandex.ru>
2025-11-26 23:15:04 +0400

    examGuestionGenerator beginning

---
+++ question-generator/src/test/java/org/skypro/question/QuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/org/skypro/question/Question.java
@@ -0,0 +1,10 @@
+package org.skypro.question;
+
+/**
+ * Модель вопроса
+ */
+public class Question {
+    private Long id;
+    private String text;
+    private String answer;
+}
